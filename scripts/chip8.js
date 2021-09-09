d807d553586dc1d7fbb730728da2dd3eb62210a9
D-Crypt <danielvaughan54@gmail.com>
2021-09-09 19:10:13 +0100

    Add init() function

---
+++ scripts/chip8.js
@@ -0,0 +1,16 @@
+import Renderer from "./renderer";
+
+const renderer = new Renderer(10);
+
+let fps = 60, loop, fpsInterval, startTime, now, then, elapsed;
+
+function init() {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+
+    renderer.testRender();
+    renderer.render();
+
+    loop = requestAnimationFrame(step);
+}
